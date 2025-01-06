a8628e792cdb04176c0a5b62de1fb097de990753
Greena1c <shchedrykserhii@gmail.com>
2025-01-06 11:48:21 +0200

    Sulution:1.2.0

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
-kind create cluster --name todoapp-cluster --config=./kind-cluster-config.yaml
+helm install todoapp .infrastructure/helm-chart/todoapp
 # Install Ingress Controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
 # kubectl apply -f .infrastructure/ingress/ingress.yml
